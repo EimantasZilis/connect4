58273ab8a09b23e9bb9542492f7d7addfe8c61cc
EimantasZilis <zilis.eimantas@gmail.com>
2021-05-15 18:31:12 +0100

    Board is populated with each player move.
    It is also possible to print board

--- connectz.py
+++ connectz.py
@@ -48,16 +48,37 @@ class GameBoard:
     ) -> None:
         self.file_object = file_object
         self.winning_moves = winning_moves
-        self.board = [[None for col in range(width)] for row in range(height)]
         self.player = cycle(range(1, 3))  #  The turn of the player (1 or 2)
+        self.total_moves = 0
+
+        # Internal representation of the game board.
+        # It is actually stored transposed to make it easier
+        # to use and access elements elsewhere in the code.
+        self.board = [[None for row in range(height)] for col in range(width)]
+        self.height = height
+
+    def check_for_wins(self):
+        pass
 
     def make_move(self, move: int):
-        pass
+        column = move - 1
+        try:
+            new_position = next(
+                x for x, psn in enumerate(self.board[column]) if psn is None
+            )
+        except StopIteration:
+            # Illegal row. The column is already full.
+            sys.exit("5")
+        except IndexError:
+            # Illegal column. Column chosen outside the board
+            sys.exit("6")
+        else:
+            self.total_moves += 1
+            self.board[column][new_position] = next(self.player)
 
     def get_player_moves(self) -> List[int]:
         """Return a list of tuples containing (Player, Move)"""
         for next_move in self.file_object:
-            self.player = next(self.player)
             next_move = next_move.rstrip("\n")
             try:
                 next_move = int(next_move)
@@ -71,6 +92,19 @@ class GameBoard:
             else:
                 yield next_move
 
+    def print_real_board(self):
+        real_board = list(map(list, zip(*self.board)))
+        self._print_board(real_board)
+
+    def print_internal_board(self):
+        self._print_board(self.board)
+
+    @staticmethod
+    def _print_board(board):
+        for row in board[::-1]:
+            print(row)
+        print()
+
 
 def main() -> None:
     arg_parser = ArgParser()
@@ -81,9 +115,17 @@ def main() -> None:
         setup = next(file_object).rstrip("\n")
         width, height, winning_moves, *_ = map(int, setup.split(" "))
 
+        if width < winning_moves and height < winning_moves:
+            # Illegal game. Game can never be won, because
+            # there are not enough rows/columns.
+            sys.exit("7")
+
         board = GameBoard(width, height, winning_moves, file_object)
         for move in board.get_player_moves():
             board.make_move(move)
+            board.check_for_wins()
+
+        board.print_real_board()
 
 
 if __name__ == "__main__":
