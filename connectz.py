d41342d89d0d701a06f2247562f968b5136ea6d8
EimantasZilis <zilis.eimantas@gmail.com>
2021-05-15 14:06:00 +0100

    added functionality for reading the game files

--- connectz.py
+++ connectz.py
@@ -9,7 +9,7 @@ class ArgParser:
     def __init__(self) -> None:
         parser = argparse.ArgumentParser(description="Connect Z")
         parser.add_argument(
-            "inputfilename", type=str, nargs="?", help="Enter the file name"
+            "inputfilename", type=str, nargs="?", help="Enter file name"
         )
         self.args = parser.parse_args()
 
@@ -40,9 +40,60 @@ class ArgParser:
             sys.exit("9")
 
 
+class ImportGame:
+    """
+    A class for importing game from file.
+    It reads the input file, validates that file contents
+    are correct and parses the information within.
+    """
+
+    def __init__(self, file: Path) -> None:
+        self.file = file
+        self.columns = None
+        self.rows = None
+        self.winning_moves = None
+
+    def __enter__(self) -> None:
+        """Read game setup from file"""
+        self.file_object = self.file.open(encoding="ascii")
+        header = self.file_object.readline().rstrip("\n")
+        setup = self.file_object.readline().rstrip("\n")
+        self.width, self.height, self.winning_moves, *bad_args = map(
+            int, setup.split(" ")
+        )
+
+        if header != "X Y Z":
+            # Invalid headers
+            sys.exit("8-1")
+        elif bad_args:
+            # There shouldn't be any more than 3 args
+            sys.exit("8-2")
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        """Close file"""
+        self.file_object.close()
+
+    def next_move(self):
+        """Generator method to read file line-by-line"""
+        # Skip the first two lines and go straight to the moves
+        next(self.file_object)
+        next(self.file_object)
+
+        for line in self.file_object:
+            stripped_line = line.rstrip("\n")
+            print(stripped_line)
+            yield int(stripped_line)
+
+
 def main() -> None:
     arg_parser = ArgParser()
-    arg_parser.get_file()
+    file = arg_parser.get_file()
+
+    with ImportGame(file) as game:
+        pass
+
+        # for move in game_setup.moves:
+        # print(move)
 
 
 if __name__ == "__main__":
