21a019d9076a5591cfa0ee8b3e3f3699a937a8ed
EimantasZilis <zilis.eimantas@gmail.com>
2021-06-17 15:43:25 +0100

    removed redundant Connect4 class

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-from pathlib import Path
-
 from game import Game
 from helpers import ArgParser
-
-
-class Connect4:
-    def __init__(self, file: Path) -> None:
-        self.file = file
-
-    def __enter__(self) -> Game:
-        self._fp = self.file.open(mode="r")
-        return Game(self._fp)
-
-    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
-        self._fp.close()
 
 
 def main() -> None:
     arg_parser = ArgParser()
     file = arg_parser.get_file()
 
-    with Connect4(file) as game:
+    with file.open(mode="r") as fp:
+        game = Game(fp)
         game.play()
         game.show_summary()
 
