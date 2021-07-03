1c969ac0129e81602a89b9cc261573f09b8b301f
EimantasZilis <zilis.eimantas@gmail.com>
2021-07-03 17:19:02 +0100

    Added more tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -25,7 +25,7 @@ class TestGameChecker:
         mock_check_lines_for_wins.assert_called_once()
 
     @patch.object(GameChecker, "_check_lines_for_wins")
-    def test_check_for_wins_make_checks(
+    def test_check_for_wins_dont_check(
         self, mock_check_lines_for_wins: MagicMock, game_checker: GameChecker
     ) -> None:
         game_checker.total_moves = 2
@@ -69,7 +69,7 @@ class TestGameChecker:
             (3, 0, [1, 0, 1, 0, None, 0, 0, 0]),
         ],
     )
-    def test_check_line_for_win_game_has_been_won(
+    def test_check_line_for_win_game_won(
         self,
         winning_moves: int,
         current_player: int,
@@ -219,19 +219,6 @@ class TestGameChecker:
         game_checker.check_horizontal_lines()
         assert game_checker.winner == None
 
-    @patch.object(GameChecker, "check_vertical_line")
-    def test_check_lines_for_wins_vertical_line_win(
-        self,
-        mock_check_vertical_line: MagicMock,
-        game_checker: GameChecker,
-    ) -> None:
-        winner = "1"
-        mock_check_vertical_line.side_effect = GameOver(winner)
-
-        assert game_checker.winner == None
-        game_checker._check_lines_for_wins()
-        assert str(game_checker.winner) == winner
-
     @pytest.mark.parametrize(
         "board,row,column,winning_moves",
         (
@@ -242,7 +229,7 @@ class TestGameChecker:
             ([[1, 0, 0, None], [0, 1, 0, None], [1, 0, 1, None]], 2, 2, 3),
         ),
     )
-    def test_check_lines_for_wins_all_scenarios(
+    def test_check_lines_for_wins_game_won(
         self,
         board: List[List[int]],
         row: int,
@@ -264,37 +251,34 @@ class TestGameChecker:
         game_checker._check_lines_for_wins()
         assert game_checker.winner == current_player
 
-    @patch.object(GameChecker, "check_vertical_line")
-    @patch.object(GameChecker, "check_left_diagonal_line")
-    @patch.object(GameChecker, "check_right_diagonal_line")
-    @patch.object(GameChecker, "check_horizontal_lines")
-    def test_check_lines_for_wins_player_1_won(
+    @pytest.mark.parametrize(
+        "board,row,column,winning_moves",
+        (
+            ([[1, 0, 1, None], [0, 1, None, None], [0, 1, None, None]], 2, 0, 3),
+            ([[1, 0, 0, 1], [0, 1, 1, None], [1, 0, 0, 1]], 3, 0, 3),
+            ([[1, 1, 0, 1], [0, 0, 1, 1], [1, 0, 1, 0]], 3, 2, 3),
+            ([[1, 0, 0, 1], [0, 1, 1, 0], [1, 0, 0, 1]], 1, 0, 3),
+            ([[None, None, None, None], [0, 1, 0, 1], [1, 0, 1, None]], 3, 1, 3),
+        ),
+    )
+    def test_check_lines_for_wins_not_won(
         self,
-        mock_check_horizontal_lines: MagicMock,
-        mock_check_right_diagonal_line: MagicMock,
-        mock_check_left_diagonal_line: MagicMock,
-        mock_check_vertical_line: MagicMock,
+        board: List[List[int]],
+        row: int,
+        column: int,
+        winning_moves: int,
         game_checker: GameChecker,
     ) -> None:
-        winner = "1"
-        mock_check_vertical_line.side_effect = GameOver(winner)
+        current_player = 1
+        game_checker.width = 3
+        game_checker.height = 4
+        game_checker.max_diagonal_length = 3
+        game_checker.winning_moves = winning_moves
+        game_checker.current_player = current_player
+        game_checker.current_column = column
+        game_checker.current_row = row
+        game_checker.board = board
 
-        assert game_checker.winner == None
-        game_checker._check_lines_for_wins()
-        assert str(game_checker.winner) == winner
-
-    @patch.object(GameChecker, "check_vertical_line")
-    @patch.object(GameChecker, "check_left_diagonal_line")
-    @patch.object(GameChecker, "check_right_diagonal_line")
-    @patch.object(GameChecker, "check_horizontal_lines")
-    def test_check_lines_for_wins_noone_won(
-        self,
-        mock_check_horizontal_lines: MagicMock,
-        mock_check_right_diagonal_line: MagicMock,
-        mock_check_left_diagonal_line: MagicMock,
-        mock_check_vertical_line: MagicMock,
-        game_checker: GameChecker,
-    ) -> None:
         assert game_checker.winner == None
         game_checker._check_lines_for_wins()
         assert game_checker.winner == None
